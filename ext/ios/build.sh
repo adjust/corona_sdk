4b4507674f4ec3ae879c0076ba2bc6318e6a2127
uerceg <ugi@adjust.com>
2017-12-21 09:57:56 +0100

    Scripts cleanup

--- ext/ios/build.sh
+++ ext/ios/build.sh
@@ -3,36 +3,66 @@
 # End script if one of the lines fails
 set -e
 
-# Get the current directory (ext/android/)
+# Colours for output
+NC='\033[0m'
+RED='\033[0;31m'
+CYAN='\033[1;36m'
+GREEN='\033[0;32m'
+
+# Validate number of input parameters
+if [ $# -ne 1 ]; then
+    echo -e "${CYAN}[ADJUST]:${RED} Missing parameter (./build.sh [debug || release])"
+    echo -e "${CYAN}[ADJUST]:${RED} Script execution aborted ${NC}"
+    exit 1
+fi
+
+# Validate input parameter value
+if [[ "$1" == "debug" ]] || [[ "$1" == "release" ]]
+then
+	BUILD_TYPE=$1
+else
+	echo -e "${CYAN}[ADJUST]:${RED} Wrong parameter '$1' (./build.sh [debug || release])"
+    echo -e "${CYAN}[ADJUST]:${RED} Script execution aborted ${NC}"
+    exit 1
+fi
+
+# Get the current directory (ext/ios)
 ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 # Traverse up to get to the root directory
 ROOT_DIR="$(dirname "$ROOT_DIR")"
 ROOT_DIR="$(dirname "$ROOT_DIR")"
-IOS_SUBMODULE_DIR=ext/ios
-FRAMEWORK_IN_DIR=sdk/build/Release-iphoneos/Static
-FRAMEWORK_OUT_DIR=Frameworks/Static
 
-RED='\033[0;31m' # Red color
-GREEN='\033[0;32m' # Green color
-NC='\033[0m' # No Color
+# iOS SDK submodule root directory
+SDK_SRC_DIR=ext/ios/sdk
 
-echo -e "${GREEN}[*build.sh*] START ${NC}"
-cd ${ROOT_DIR}/${IOS_SUBMODULE_DIR}
+# Output directory where static library and header files will be copied to.
+OUTPUT_DIR=plugin/ios/Plugin
 
-# Clean the folders
-echo -e "${GREEN}[*build.sh*] Cleaning <${FRAMEWORK_OUT_DIR}> directory ${NC}"
-rm -rf ${FRAMEWORK_OUT_DIR}
+# Path in which static framework will be generated.
+STATIC_FRAMEWORK_DIR=$SDK_SRC_DIR/Frameworks/Static/AdjustSdk.framework
 
-# Create needed folders
-mkdir -p ${FRAMEWORK_OUT_DIR}
+echo -e "${CYAN}[ADJUST]:${GREEN} START of Corona iOS submodule build.sh script ${NC}"
 
-echo -e "${GREEN}[*build.sh*] Running xcodebuild ${NC}"
-cd ${ROOT_DIR}/${IOS_SUBMODULE_DIR}/sdk
-xcodebuild -target AdjustStatic -configuration Release clean build
+# Build static framework
+echo -e "${CYAN}[ADJUST]:${GREEN} Building static framework ... ${NC}"
+cd ${ROOT_DIR}/${SDK_SRC_DIR}
 
-echo -e "${GREEN}[*build.sh*] Moving the framework from ${FRAMEWORK_IN_DIR} to ${FRAMEWORK_OUT_DIR} ${NC}"
-cd ${ROOT_DIR}/${IOS_SUBMODULE_DIR}
-mv -v ${FRAMEWORK_IN_DIR}/*.framework ${ROOT_DIR}/${IOS_SUBMODULE_DIR}/${FRAMEWORK_OUT_DIR}/AdjustSdk.framework
+if [ "$BUILD_TYPE" == "debug" ]; then
+	xcodebuild -target AdjustStatic -configuration Debug clean build
+elif [ "$BUILD_TYPE" == "release" ]; then
+	xcodebuild -target AdjustStatic -configuration Release clean build
+fi
+echo -e "${CYAN}[ADJUST]:${GREEN} Done! ${NC}"
 
-echo -e "${GREEN}[*build.sh*] DONE ${NC}"
+# Copy static library from generated framework to output directory
+echo -e "${CYAN}[ADJUST]:${GREEN} Copying static library from generated framework to output directory ... ${NC}"
+cp -v ${ROOT_DIR}/${STATIC_FRAMEWORK_DIR}/Versions/A/AdjustSdk ${ROOT_DIR}/${OUTPUT_DIR}/AdjustSdk.a
+echo -e "${CYAN}[ADJUST]:${GREEN} Done! ${NC}"
+
+# Copy public headers from generated framework to output directory
+echo -e "${CYAN}[ADJUST]:${GREEN} Copying headers from generated framework to output directory ... ${NC}"
+cp -v ${ROOT_DIR}/${STATIC_FRAMEWORK_DIR}/Versions/A/Headers/* ${ROOT_DIR}/${OUTPUT_DIR}
+echo -e "${CYAN}[ADJUST]:${GREEN} Done! ${NC}"
+
+echo -e "${CYAN}[ADJUST]:${GREEN} END of Corona iOS submodule build.sh script execution ${NC}"
