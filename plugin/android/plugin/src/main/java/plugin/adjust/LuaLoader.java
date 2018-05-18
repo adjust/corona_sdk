fd4b9ead418677e4973526601d64b0039a273e83
uerceg <ugi@adjust.com>
2018-05-18 12:39:51 +0200

    New version 4.13.1

--- plugin/android/plugin/src/main/java/plugin/adjust/LuaLoader.java
+++ plugin/android/plugin/src/main/java/plugin/adjust/LuaLoader.java
@@ -348,7 +348,7 @@ public class LuaLoader implements JavaFunction, CoronaRuntimeListener {
         L.pop(1);
 
         // SDK prefix
-        adjustConfig.setSdkPrefix("corona4.13.0");
+        adjustConfig.setSdkPrefix("corona4.13.1");
 
         // Main process name
         L.getField(1, "processName");
