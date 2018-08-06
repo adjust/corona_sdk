7a29d89d4021dc23e0aa29ecb5b09aadbf7bbf38
serj <srdjan@adjust.com>
2018-08-06 16:33:09 +0200

    test app - example code removed; cleanup

--- test/Corona/main.lua
+++ test/Corona/main.lua
@@ -1,4 +1,4 @@
-local adjust = require "plugin.adjust"
+--local adjust = require "plugin.adjust"
 local testLib = require "plugin.testlibrary"
 local widget = require "widget"
 local json = require "json"
@@ -21,111 +21,6 @@ if launchArgs and launchArgs.url then
     --adjust.appWillOpenUrl(launchArgs.url)
 end
 
--- Setup listeners
--- ------------------------
-local function attributionListener(event)
-    local json_attribution = json.decode(event.message)
-    print("[Adjust]: Attribution changed!")
-    print("[Adjust]: Tracker token: " .. json_attribution.trackerToken)
-    print("[Adjust]: Tracker name: " .. json_attribution.trackerName)
-    print("[Adjust]: Campaign: " .. json_attribution.campaign)
-    print("[Adjust]: Network: " .. json_attribution.network)
-    print("[Adjust]: Creative: " .. json_attribution.creative)
-    print("[Adjust]: Adgroup: " .. json_attribution.adgroup)
-    print("[Adjust]: ADID: " .. json_attribution.adid)
-end
-
-local function sessionTrackingSuccessListener(event)
-    local json_session_success = json.decode(event.message)
-    print("[Adjust]: Session tracking success!")
-    print("[Adjust]: Message: " .. json_session_success.message)
-    print("[Adjust]: Timestamp: " .. json_session_success.timestamp)
-    print("[Adjust]: Adid: " .. json_session_success.adid)
-    print("[Adjust]: JSON response: " .. json_session_success.jsonResponse)
-end
-
-local function sessionTrackingFailureListener(event)
-    local json_session_failure = json.decode(event.message)
-    print("[Adjust]: Session tracking failure!")
-    print("[Adjust]: Message: " .. json_session_failure.message)
-    print("[Adjust]: Timestamp: " .. json_session_failure.timestamp)
-    print("[Adjust]: Adid: " .. json_session_failure.adid)
-    print("[Adjust]: Will retry: " .. json_session_failure.adid)
-    print("[Adjust]: JSON response: " .. json_session_failure.jsonResponse)
-end
-
-local function eventTrackingSuccessListener(event)
-    local json_event_success = json.decode(event.message)
-    print("[Adjust]: Event tracking success!")
-    print("[Adjust]: Event token: " .. json_event_success.eventToken)
-    print("[Adjust]: Message: " .. json_event_success.message)
-    print("[Adjust]: Timestamp: " .. json_event_success.timestamp)
-    print("[Adjust]: Adid: " .. json_event_success.adid)
-    print("[Adjust]: JSON response: " .. json_event_success.jsonResponse)
-end
-
-local function eventTrackingFailureListener(event)
-    local json_event_failure = json.decode(event.message)
-    print("[Adjust]: Event tracking failure!")
-    print("[Adjust]: Event token: " .. json_event_failure.eventToken)
-    print("[Adjust]: Message: " .. json_event_failure.message)
-    print("[Adjust]: Timestamp: " .. json_event_failure.timestamp)
-    print("[Adjust]: Adid: " .. json_event_failure.adid)
-    print("[Adjust]: Will retry: " .. json_event_failure.willRetry)
-    print("[Adjust]: JSON response: " .. json_event_failure.jsonResponse)
-end
-
-local function deferredDeeplinkListener(event)
-    print("[Adjust]: Received event from deferredDeeplinkListener (" .. event.name .. "): ", event.message)
-end
-
---adjust.setAttributionListener(attributionListener)
---adjust.setEventTrackingSuccessListener(eventTrackingSuccessListener)
---adjust.setEventTrackingFailureListener(eventTrackingFailureListener)
---adjust.setSessionTrackingSuccessListener(sessionTrackingSuccessListener)
---adjust.setSessionTrackingFailureListener(sessionTrackingFailureListener)
---adjust.setDeferredDeeplinkListener(deferredDeeplinkListener)
-
--- Init Adjust
--- ------------------------
---adjust.addSessionCallbackParameter("scp1", "scp1_value1")
---adjust.addSessionCallbackParameter("scp2", "scp2_value2")
---adjust.addSessionCallbackParameter("scp3", "scp3_value3")
---
---adjust.removeSessionCallbackParameter("scp2")
---
---adjust.resetSessionCallbackParameters()
---
---adjust.addSessionPartnerParameter("spp1", "spp1_value1")
---adjust.addSessionPartnerParameter("spp2", "spp2_value2")
---adjust.addSessionPartnerParameter("spp3", "spp3_value3")
---
---adjust.removeSessionPartnerParameter("spp1")
---
---adjust.resetSessionPartnerParameters()
-
---adjust.create({
---    appToken = "2fm9gkqubvpc",
---    environment = "SANDBOX",
---    logLevel = "VERBOSE",
---    -- shouldLaunchDeeplink = false,
---    -- eventBufferingEnabled = true,
---    -- delayStart = 6.0,
---    -- isDeviceKnown = true,
---    -- sendInBackground = true,
---    -- defaultTracker = "abc123",
---    -- userAgent = "Random User Agent 6.6"
---    -- readMobileEquipmentIdentity = true
---    -- secretId = aaa,
---    -- info1 = bbb,
---    -- info2 = ccc,
---    -- info3 = ddd,
---    -- info4 = eee,
---})
-
--- adjust.setPushToken("{YourPushToken}")
--- adjust.sendFirstPackages()
-
 -- Setting up assets
 -- ------------------------
 display.setDefault("background", 1, 1, 1)
@@ -135,7 +30,7 @@ local baseUrl = "https://192.168.8.103:8443"
 local function executeCommand(event)
     local command = json.decode(event.message)
     print("  >>>>> Executing command: " .. command.className .. "." .. command.methodName .. " <<<<<")
-    
+    -- TODO: create and use AdjustCommandExecutor
     
 end
 
@@ -154,63 +49,11 @@ end
 
 widget.newButton({
     left = display.contentCenterX - 85,
-    top = 40 + (3 * 30),
-    id = "button2",
+    top = 40 + (0 * 30),
+    id = "button0",
     label = "Start Test Session",
     onEvent = handleStartTestSession
 })
 
--- Test Func
--- ------------------------
-local function handleTestFunc(event)
-    if ("ended" == event.phase) then
-        testLib.testFunc()
-    end
-end
-
-widget.newButton({
-    left = display.contentCenterX - 85,
-    top = 40 + (5 * 30),
-    id = "button3",
-    label = "Test Func",
-    onEvent = handleTestFunc
-})
-
--- Track Revenue Event
--- ------------------------
-local function handleTrackSimpleEvent(event)
-    if ("ended" == event.phase) then
-        adjust.trackEvent({
-            eventToken = "g3mfiw"
-        })
-    end
-end
-
-widget.newButton({
-    left = display.contentCenterX - 85,
-    top = 40 + (0 * 30),
-    id = "button0",
-    label = "Track Simple Event",
-    onEvent = handleTrackSimpleEvent
-})
-
--- Track Revenue Event
--- ------------------------
-local function handleTrackRevenueEvent(event)
-    if ("ended" == event.phase) then
-        adjust.trackEvent({
-            eventToken = "a4fd35",
-            revenue = 0.01,
-            currency = "EUR",
-            transactionId = "some_transaction_id"
-        })
-    end
-end
-
-widget.newButton({
-    left = display.contentCenterX - 85,
-    top = 40 + (1 * 30),
-    id = "button1",
-    label = "Track Revenue Event",
-    onEvent = handleTrackRevenueEvent
-})
+-- START TEST SESSION AUTIMATICALLY
+testLib.startTestSession("corona4.14.0@android4.14.0")
--- test/android/plugin/src/main/java/plugin/testlibrary/LuaLoader.java
+++ test/android/plugin/src/main/java/plugin/testlibrary/LuaLoader.java
@@ -42,12 +42,6 @@ import java.util.Map;
 public class LuaLoader implements JavaFunction, CoronaRuntimeListener {
 	private static final String TAG = "LuaLoader-TestLib";
 
-	/** Lua registry ID to the Lua function to be called when the ad request finishes. */
-	private int fListener;
-
-	/** This corresponds to the event name, e.g. [Lua] event.name */
-	private static final String EVENT_NAME = "pluginlibraryevent";
-
 	private TestLibrary testLibrary;
 	private int executeCommandListener;
 
@@ -60,8 +54,6 @@ public class LuaLoader implements JavaFunction, CoronaRuntimeListener {
 	 */
 	@SuppressWarnings("unused")
 	public LuaLoader() {
-		// Initialize member variables.
-		fListener = CoronaLua.REFNIL;
 		executeCommandListener = CoronaLua.REFNIL;
 
 		// Set up this plugin to listen for Corona runtime events to be received by methods
@@ -85,15 +77,12 @@ public class LuaLoader implements JavaFunction, CoronaRuntimeListener {
 	public int invoke(LuaState L) {
 		// Register this plugin into Lua with the following functions.
 		NamedJavaFunction[] luaFunctions = new NamedJavaFunction[] {
-			new InitWrapper(),
-			new ShowWrapper(),
 			new InitTestLibraryWrapper(),
 			new AddTestWrapper(),
 			new AddTestDirectoryWrapper(),
 			new StartTestSessionWrapper(),
 			new AddInfoToSendWrapper(),
 			new SendInfoToServerWrapper(),
-			new TestFuncWrapper(),
 		};
 		String libName = L.toString( 1 );
 		L.register(libName, luaFunctions);
@@ -162,37 +151,8 @@ public class LuaLoader implements JavaFunction, CoronaRuntimeListener {
 	 */
 	@Override
 	public void onExiting(CoronaRuntime runtime) {
-		// Remove the Lua listener reference.
-		CoronaLua.deleteRef( runtime.getLuaState(), fListener );
 		CoronaLua.deleteRef(runtime.getLuaState(), executeCommandListener);
-
-		fListener = CoronaLua.REFNIL;
 		executeCommandListener = CoronaLua.REFNIL;
-	}
-
-	/**
-	 * Simple example on how to dispatch events to Lua. Note that events are dispatched with
-	 * Runtime dispatcher. It ensures that Lua is accessed on it's thread to avoid race conditions
-	 * @param message simple string to sent to Lua in 'message' field.
-	 */
-	@SuppressWarnings("unused")
-	public void dispatchEvent(final String message) {
-		CoronaEnvironment.getCoronaActivity().getRuntimeTaskDispatcher().send( new CoronaRuntimeTask() {
-			@Override
-			public void executeUsing(CoronaRuntime runtime) {
-				LuaState L = runtime.getLuaState();
-
-				CoronaLua.newEvent( L, EVENT_NAME );
-
-				L.pushString(message);
-				L.setField(-2, "message");
-
-				try {
-					CoronaLua.dispatchEvent( L, fListener, 0 );
-				} catch (Exception ignored) {
-				}
-			}
-		} );
 	}
 
 	private void dispatchEvent(final LuaState luaState, final int listener, final String name, final String message) {
@@ -218,147 +178,6 @@ public class LuaLoader implements JavaFunction, CoronaRuntimeListener {
 		});
 	}
 
-	/**
-	 * The following Lua function has been called:  library.init( listener )
-	 * <p>
-	 * Warning! This method is not called on the main thread.
-	 * @param L Reference to the Lua state that the Lua function was called from.
-	 * @return Returns the number of values to be returned by the library.init() function.
-	 */
-	@SuppressWarnings({"WeakerAccess", "SameReturnValue"})
-	public int init(LuaState L) {
-		int listenerIndex = 1;
-
-		if ( CoronaLua.isListener( L, listenerIndex, EVENT_NAME ) ) {
-			fListener = CoronaLua.newRef( L, listenerIndex );
-		}
-
-		return 0;
-	}
-
-	/**
-	 * The following Lua function has been called:  library.show( word )
-	 * <p>
-	 * Warning! This method is not called on the main thread.
-	 * @param L Reference to the Lua state that the Lua function was called from.
-	 * @return Returns the number of values to be returned by the library.show() function.
-	 */
-	@SuppressWarnings("WeakerAccess")
-	public int show(LuaState L) {
-		// Fetch a reference to the Corona activity.
-		// Note: Will be null if the end-user has just backed out of the activity.
-		CoronaActivity activity = CoronaEnvironment.getCoronaActivity();
-		if (activity == null) {
-			return 0;
-		}
-
-		// Fetch the first argument from the called Lua function.
-		String word = L.checkString( 1 );
-		if ( null == word ) {
-			word = "corona";
-		}
-
-		// Create web view on the main UI thread.
-		final String url = "http://dictionary.reference.com/browse/" + word;
-		activity.runOnUiThread(new Runnable() {
-			@Override
-			public void run() {
-				// Fetch a reference to the Corona activity.
-				// Note: Will be null if the end-user has just backed out of the activity.
-				CoronaActivity activity = CoronaEnvironment.getCoronaActivity();
-				if (activity == null) {
-					return;
-				}
-
-				// Create and set up the web view.
-				WebView view = new WebView(activity);
-
-				// Prevent redirect which causes an external browser to be launched
-				// because some sites detect phone/tablet and redirect.
-				view.setWebViewClient(new WebViewClient() {
-					@Override
-					public boolean shouldOverrideUrlLoading(WebView view, String url) {
-						return false;
-					}
-				});
-
-				// Display the web view.
-				activity.getOverlayView().addView(view);
-				view.loadUrl( url );
-			}
-		} );
-
-
-		// This will send event via dispatchEvent in 5 seconds. Not that dispatchEvent is called from
-		// background thread, but it will use runtime dispatcher to select proper thread for Lua message dispatch
-		(new Thread() {
-			@Override
-			public void run() {
-				// Sleep for 5 seconds
-				try {
-					Thread.sleep(5000);
-				} catch (InterruptedException ignored) {}
-
-				// dispatch message!
-				dispatchEvent("Hello!");
-			}
-		}).start();
-
-		return 0;
-	}
-
-	/** Implements the library.init() Lua function. */
-	@SuppressWarnings("unused")
-	private class InitWrapper implements NamedJavaFunction {
-		/**
-		 * Gets the name of the Lua function as it would appear in the Lua script.
-		 * @return Returns the name of the custom Lua function.
-		 */
-		@Override
-		public String getName() {
-			return "init";
-		}
-		
-		/**
-		 * This method is called when the Lua function is called.
-		 * <p>
-		 * Warning! This method is not called on the main UI thread.
-		 * @param L Reference to the Lua state.
-		 *                 Needed to retrieve the Lua function's parameters and to return values back to Lua.
-		 * @return Returns the number of values to be returned by the Lua function.
-		 */
-		@Override
-		public int invoke(LuaState L) {
-			return init(L);
-		}
-	}
-
-	/** Implements the library.show() Lua function. */
-	@SuppressWarnings("unused")
-	private class ShowWrapper implements NamedJavaFunction {
-		/**
-		 * Gets the name of the Lua function as it would appear in the Lua script.
-		 * @return Returns the name of the custom Lua function.
-		 */
-		@Override
-		public String getName() {
-			return "show";
-		}
-		
-		/**
-		 * This method is called when the Lua function is called.
-		 * <p>
-		 * Warning! This method is not called on the main UI thread.
-		 * @param L Reference to the Lua state.
-		 *                 Needed to retrieve the Lua function's parameters and to return values back to Lua.
-		 * @return Returns the number of values to be returned by the Lua function.
-		 */
-		@Override
-		public int invoke(LuaState L) {
-			return show(L);
-		}
-	}
-
 	///////////////////////////////////////////////////////////////////////////////////////////////////
 	/////////// TESTAPP CODE //////////////////////////////////////////////////////////////////////////
 	///////////////////////////////////////////////////////////////////////////////////////////////////
@@ -367,12 +186,7 @@ public class LuaLoader implements JavaFunction, CoronaRuntimeListener {
 
 		String baseUrl = L.checkString(1);
 
-		if (CoronaLua.isListener(L, 1, "ADJUST")) {
-			Log.d(TAG, "Index 1 is listener!");
-		}
-
 		if (CoronaLua.isListener(L, 2, "ADJUST")) {
-			Log.d(TAG, "Index 2 is listener!");
 			this.executeCommandListener = CoronaLua.newRef(L, 2);
 		}
 
@@ -450,13 +264,6 @@ public class LuaLoader implements JavaFunction, CoronaRuntimeListener {
 		return 0;
 	}
 
-	private int testLib_testFunc(LuaState L) {
-		Log.d(TAG, "Test library test Func called!");
-		Log.d(TAG, "Test library test Func called!");
-		Log.d(TAG, "Test library test Func called!");
-		return 0;
-	}
-
 	private class InitTestLibraryWrapper implements NamedJavaFunction {
 		@Override
 		public String getName() {
@@ -526,18 +333,6 @@ public class LuaLoader implements JavaFunction, CoronaRuntimeListener {
 		@Override
 		public int invoke(LuaState L) {
 			return testLib_sendInfoToServer(L);
-		}
-	}
-
-	private class TestFuncWrapper implements NamedJavaFunction {
-		@Override
-		public String getName() {
-			return "testFunc";
-		}
-
-		@Override
-		public int invoke(LuaState L) {
-			return testLib_testFunc(L);
 		}
 	}
 
