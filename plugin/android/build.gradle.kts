7739f8ef1c4204ac6136e11155bd9d18e273ad45
uerceg <ugi@adjust.com>
2025-08-12 23:19:22 +0200

    build: update android tooling for plugin and example app

--- plugin/android/build.gradle.kts
+++ plugin/android/build.gradle.kts
@@ -5,8 +5,8 @@ buildscript {
     }
     dependencies {
         classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.7.10")
-        classpath("com.android.tools.build:gradle:8.8.0")
-        classpath("com.beust:klaxon:5.5")
+        classpath("com.android.tools.build:gradle:8.12.0")
+        classpath("com.beust:klaxon:5.6")
     }
 }
 
